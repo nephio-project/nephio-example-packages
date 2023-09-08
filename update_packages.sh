3fc0049a7034efe3fcb28b4cac45273a84609672
Victor Morales <v.morales@samsung.com>
2023-09-08 12:02:25 -0700

    Fix CLUSTER_TOPOLOGY var in update packages script
    Signed-off-by: Victor Morales <v.morales@samsung.com>

--- update_packages.sh
+++ update_packages.sh
@@ -42,6 +42,7 @@ CLUSTER_API_VERSION="v$(get_github_latest_release kubernetes-sigs/cluster-api)"
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
 CLUSTERCMD_PATH="$(pwd)/bin/clusterctl"
+export CLUSTER_TOPOLOGY=true
 
 if [ ! -f "$CLUSTERCMD_PATH" ] || [ "$("$CLUSTERCMD_PATH" version -o short)" != "$CLUSTER_API_VERSION" ]; then
     mkdir -p "$(dirname "$CLUSTERCMD_PATH")"
@@ -52,4 +53,4 @@ fi
 "$CLUSTERCMD_PATH" generate provider --core cluster-api --write-to cluster-capi/cluster-api-core.yaml
 "$CLUSTERCMD_PATH" generate provider --bootstrap kubeadm --write-to cluster-capi/cluster-api-bootstrap.yaml
 "$CLUSTERCMD_PATH" generate provider --control-plane kubeadm --write-to cluster-capi/cluster-api-control-plane.yaml
-CLUSTER_TOPOLOGY=true "$CLUSTERCMD_PATH" generate provider --infrastructure docker --write-to cluster-capi-infrastructure-docker/cluster-api-infrastructure-docker.yaml
+"$CLUSTERCMD_PATH" generate provider --infrastructure docker --write-to cluster-capi-infrastructure-docker/cluster-api-infrastructure-docker.yaml
